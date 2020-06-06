616d47c272416678d087164e484465fb67c5dc04
Rafael Brayner Miranda de Carvalho <rafael.carvalho@pmjppaco.local>
2020-06-05 22:09:46 -0300

    Adiciona opcao de delete

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,17 +1,28 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import { Container, Table, Button, Row } from "react-bootstrap";
 import axios from "axios";
 import * as moment from "moment"
+import withReactContent from "sweetalert2-react-content";
+import Swal from "sweetalert2";
+import { toast } from 'react-toastify';
 
 const url = "https://node-todo-dev.herokuapp.com/api/todos";
 
+
 export default (props) => {
 
+  const history = useHistory();
+  const MySwal = withReactContent(Swal);
   const [todos, setTodos] = useState([])
 
   useEffect(() => {
     findTodos();
   }, []);
+
+  function goToCreate() {
+    history.push("/create");
+  }
 
   async function findTodos() {
     try {
@@ -22,6 +33,31 @@ export default (props) => {
     }
   }
 
+  async function handleRemove(id) {
+    try {
+      const shouldDelete = await MySwal.fire({
+        title: "Você tem certeza?",
+        text: "Uma vez deletado, você não poderá recuperar este TODO.",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#d9534f",
+        confirmButtonText: "Sim",
+        cancelButtonText: "Não",
+      });
+      if(shouldDelete.value) {
+        /*const res = await axios.delete(`${url}/${id}`);
+        console.log(res)
+        if(res.status === 201) {
+          toast("TODO removido com sucesso!")
+        }*/
+      }
+      
+    } catch (error) {
+      console.error(error);
+    }
+    
+  }
+
   function mapTodos(todos) {
     return todos.map(
       todo => (
@@ -29,6 +65,7 @@ export default (props) => {
           <td>{todo.description}</td>
           <td>{moment(todo.createdAt).format("DD/MM/YYYY")}</td>
           <td>{todo.done ? "Sim" : "Não"}</td>
+          <td><Button variant="danger" onClick={handleRemove}>Delete</Button></td>
         </tr>
       )
     )
@@ -39,7 +76,7 @@ export default (props) => {
       <Container>
         <Row>
           <h3>TODOs</h3>
-          <Button variant="primary">Create</Button>
+          <Button onClick={goToCreate} variant="primary">Create</Button>
         </Row>
 
         <Table striped bordered hover>
@@ -48,6 +85,7 @@ export default (props) => {
               <th>Descrição</th>
               <th>Data de Criação</th>
               <th>Concluído</th>
+              <th>Ação</th>
             </tr>
           </thead>
           <tbody>
